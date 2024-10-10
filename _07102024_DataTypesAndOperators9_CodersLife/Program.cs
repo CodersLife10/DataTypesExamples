7947528abde3b04bd5a46afbb8c57a7b7723fc60
Ahmet Burak <ahmetbrkcdrlife@gmail.com>
2024-10-10 16:47:12 +0300

    BaseSettings

---
+++ _07102024_DataTypesAndOperators9_CodersLife/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07102024_DataTypesAndOperators9_CodersLife
+{
+    internal class Program
+    {//----------> [VERİ TİPLERİ VE OPERATÖRLERİ - 5. KISIM FINAL PROJELERI] <----------//
+        /*
+         * <------------------------------------------[Aritmetik Operatörler]------------------------------------------>
+         * '+' : Toplama
+         * '-' : Çıkarma
+         * '*' : Çarpma
+         * '/' : Bölme
+         * '%' : Mod (Kalan)
+         * '++' : Bir artırma
+         * '--' : Bir azaltma
+         * <------------------------------------------[Atama Operatörleri]------------------------------------------>     
+         *  '=' : Atama
+         *  '+=' : Toplama ve atama
+         *  '-=' : Çıkarma ve atama
+         *  '*=' : Çarpma ve atama
+         *  '/=' : Bölme ve atama
+         *  '%=' : Mod alma ve atama
+         * <---------------------------------------[Karşılaştırma Operatörleri]--------------------------------------->
+         *  '==' : Eşitlik
+         *  '!=' : Eşit olmama
+         *  '>' : Büyüktür
+         *  '<' : Küçüktür
+         *  '>=' : Büyük veya eşit
+         *  '<=' : Küçük veya eşit
+         * <---------------------------------------[Mantıksal Operatörler]--------------------------------------->
+         *  '&&' : Mantıksal VE
+         *  '||' : Mantıksal VEYA
+         *  '!' : Mantıksal DEĞİL
+         */
+
+        #region Ornek
+        // HEADER: Kullanıcıdan alınan bilginin 
+        #endregion
+
+        #region Ornek1
+        #endregion
+
+        #region Ornek2
+        #endregion
+
+        #region Ornek3
+        #endregion
+
+        #region Ornek4
+        #endregion
+
+        #region Ornek5
+        #endregion
+
+        #region Ornek6
+        #endregion
+
+        #region Ornek7
+        #endregion
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
