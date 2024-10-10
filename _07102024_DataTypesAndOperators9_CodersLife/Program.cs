59e86917e14663b679272270c0db28cc4741489d
Ahmet Burak <ahmetbrkcdrlife@gmail.com>
2024-10-10 18:13:39 +0300

    Operator code samples added.

--- _07102024_DataTypesAndOperators9_CodersLife/Program.cs
+++ _07102024_DataTypesAndOperators9_CodersLife/Program.cs
@@ -37,33 +37,73 @@ namespace _07102024_DataTypesAndOperators9_CodersLife
          *  '!' : Mantıksal DEĞİL
          */
 
+        //NOT: HER ÖRNEKTE ÖNCELİKLE KULLANICIYA BİR BİLGİ ÇIKTISI VERİLECEK.
         #region Ornek
-        // HEADER: Kullanıcıdan alınan bilginin 
+        // HEADER: Kullanıcıdan iki sayı alıp toplama ve çıkarma işlemi yap.
+        // TODO:
+        // 1. İki sayı girişi alınacak.
+        // 2. Toplama ve çıkarma işlemleri yapılacak.
+        // 3. Sonuçlar ekrana yazdırılacak.
         #endregion
 
         #region Ornek1
+        // HEADER: Kullanıcıdan iki sayı alıp çarpma, bölme ve mod işlemi yap.
+        // TODO:
+        // 1. İki sayı girişi alınacak.
+        // 2. Çarpma, bölme ve mod işlemleri yapılacak.
+        // 3. Sonuçlar konsola yazdırılacak.
         #endregion
 
         #region Ornek2
+        // HEADER: Bir sayıya önce normal atama, sonra += ve -= operatörlerini uygula.
+        // TODO:
+        // 1. Kullanıcıdan bir sayı alınacak.
+        // 2. '+=' ve '-=' operatörleri uygulanacak.
+        // 3. Sonuçlar ekranda gösterilecek.
         #endregion
 
         #region Ornek3
+        // HEADER: Bir sayıya '*=' ve '/=' operatörleri uygulayarak sonucu göster.
+        // TODO:
+        // 1. Kullanıcıdan bir sayı alınacak.
+        // 2. Çarpma ve bölme ile atama operatörleri uygulanacak.
+        // 3. Sonuçlar ekrana yazdırılacak.
         #endregion
 
         #region Ornek4
+        // HEADER: İki sayı alıp eşitlik ve büyüklük karşılaştırması yap.
+        // TODO:
+        // 1. İki sayı girişi alınacak.
+        // 2. '==' ve '>' karşılaştırma operatörleri ile kıyaslama yapılacak.
+        // 3. Sonuçlar ekrana yazdırılacak.
         #endregion
 
         #region Ornek5
+        // HEADER: İki sayı alıp küçük veya eşit ve eşit olmama karşılaştırması yap.
+        // TODO:
+        // 1. İki sayı girişi alınacak.
+        // 2. '<=' ve '!=' operatörleri ile kıyaslama yapılacak.
+        // 3. Sonuçlar ekranda gösterilecek.
         #endregion
 
         #region Ornek6
+        // HEADER: İki mantıksal ifade alıp VE operatörü ile sonucu göster.
+        // TODO:
+        // 1. İki mantıksal değer (true/false) alınacak.
+        // 2. '&&' operatörü ile işlem yapılacak.
+        // 3. Sonuç konsola yazdırılacak.
         #endregion
 
         #region Ornek7
+        // HEADER: Bir mantıksal ifadeye DEĞİL operatörünü uygula ve sonucu göster.
+        // TODO:
+        // 1. Bir mantıksal değer (true/false) alınacak.
+        // 2. '!' operatörü uygulanacak.
+        // 3. Sonuç ekrana yazdırılacak.
         #endregion
-        static void Main(string[] args)
-        {
 
+        static void Main(string[] args)
+        {            
         }
     }
 }
